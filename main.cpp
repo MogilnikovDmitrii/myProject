5d30980990bcb2d6dc2a4f2a7084b3dc2fd0d80b
dima <mogilnikovdmitrij12@gmail.com>
2025-09-19 20:13:59 +0300

    19 sep

--- main.cpp
+++ main.cpp
@@ -3,8 +3,34 @@
 using namespace std;
 
 int main() {
-	cout << "Hello world";
+	int a,b;
+	double result;
+	char oper;
+	cin >> a >> b;
+	cin >> oper;
+	switch (oper)
+	{
+	case '+':
+		result = a + b;
+		break;
+	case '-':
+		result = a - b;
+		break;
+	case '*':
+		result = a * b;
+		break;
+	case '/':
+		if (b != 0) {
+			result = a / b;
+		} else {
+			cout << "Ошибка: деление на ноль";
+			return 0;
+		}
+		break;
+	default:
+		cout << "Неверный оператор";
+		return 0;
+	}
+	cout << result;
 	return 0;
-
-
 }
